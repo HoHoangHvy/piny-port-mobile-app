f1633ba1c42b78ba343ffced08e3efd93e84265c
duckh4ng28 <12a2nguyenduckhang@gmail.com>
2024-10-23 14:11:20 +0700

    Add Chat fragment

---
+++ app/src/main/java/com/example/pinyport/ui/chat/ChatAdapter.java
@@ -0,0 +1,57 @@
+package com.example.pinyport.ui.chat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pinyport.R;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    private List<Chat> chatList;
+
+    public ChatAdapter(List<Chat> chatList) {
+        this.chatList = chatList;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the item layout for each chat item
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat, parent, false);
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        // Bind data to the views
+        Chat chat = chatList.get(position);
+        holder.customerName.setText(chat.getName());
+        holder.latestChat.setText(chat.getLatestMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatList.size();
+    }
+
+    public class ChatViewHolder extends RecyclerView.ViewHolder {
+        TextView customerName, latestChat;
+        ImageView profileImage;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // Initialize views
+            customerName = itemView.findViewById(R.id.customerName);
+            latestChat = itemView.findViewById(R.id.latestChat);
+            profileImage = itemView.findViewById(R.id.profileImage);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pinyport/ui/chat/ItemDecoration.java
@@ -0,0 +1,25 @@
+package com.example.pinyport.ui.chat;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemDecoration extends RecyclerView.ItemDecoration {
+    private final int verticalSpaceHeight;
+
+    public ItemDecoration(int verticalSpaceHeight) {
+        this.verticalSpaceHeight = verticalSpaceHeight;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+
+        // Set khoảng cách cho tất cả các item trừ item cuối cùng
+        if (parent.getChildAdapterPosition(view) != parent.getAdapter().getItemCount() - 1) {
+            outRect.bottom = verticalSpaceHeight;
+        }
+    }
+}
