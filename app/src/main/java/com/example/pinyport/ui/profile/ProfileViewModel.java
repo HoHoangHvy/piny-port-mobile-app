8c694359fd0b15b4888843ad9af586ac5f9d8941
hohoanghvy <huyhohoang1907@gmail.com>
2024-10-24 14:12:59 +0700

    merge code kape

--- app/src/main/java/com/example/pinyport/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/pinyport/ui/profile/ProfileViewModel.java
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+
+
 public class ProfileViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
